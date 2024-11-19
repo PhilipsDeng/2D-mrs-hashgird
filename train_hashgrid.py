8ea36954b4ad3b0a470d57c97732c85a6be3f30b
PhilipsDeng <philips.deng@tum.de>
2024-11-19 03:15:51 +0100

    Much faster training, dynamic resolution inference,

--- train_hashgrid.py
+++ train_hashgrid.py
@@ -11,14 +11,13 @@ import matplotlib.pyplot as plt
 import os
 from PIL import Image
 import torchvision.transforms as transforms
-
+torch.backends.cudnn.benchmark = True
 
 # 加载 config.yaml 文件
 def load_config(config_path):
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
-
 
 # 指定设备
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -27,7 +26,8 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 # 加载目标图像并转换为张量
 def load_target_image(image_path, resolution):
     transform = transforms.Compose([
-        transforms.Resize((resolution, resolution)),
+        transforms.Lambda(lambda img: transforms.functional.crop(img, 0, 0, min(img.size), min(img.size))),  
+        transforms.Resize((resolution, resolution)),  # 调整大小到指定分辨率
         transforms.ToTensor(),  # 将图像转换为 [0, 1] 范围内的张量
     ])
     image = Image.open(image_path).convert('RGB')
@@ -38,8 +38,9 @@ def load_target_image(image_path, resolution):
 # 训练函数
 def train(config):
     # 创建模型和优化器
-    hashgrid = HashGrid(config['hashgrid_config']).to(device)
-    renderer = Renderer.hash_render(config['renderer_config']).to(device)
+    dtype = getattr(torch, config['dtype'])
+    hashgrid = HashGrid(config['hashgrid_config']).to(device).to(dtype)
+    renderer = Renderer.hash_render(config['renderer_config']).to(device).to(dtype)
     optimizer = optim.Adam([
         {'params': hashgrid.parameters()},
         {'params': renderer.parameters()}
@@ -48,70 +49,80 @@ def train(config):
     # 创建训练数据
     x, y = torch.arange(config['resolution']) / (config['resolution'] - 1) * 2 - 1, torch.arange(config['resolution']) / (config['resolution'] - 1) * 2 - 1
     x, y = torch.meshgrid(x, y)
-    inputs = torch.stack([x, y], dim=-1).to(device).float()
-    target_img = load_target_image('data/001.png', config['resolution'])  # 加载目标图像
+    inputs = torch.stack([x, y], dim=-1).to(device).to(dtype)
+    target_img = load_target_image('data/001.png', config['resolution'])  
+    target = torch.permute(target_img, (1, 2, 0)).reshape(-1, 3).to(device).to(dtype)
 
-    # 训练循环
+    # 训练
     for epoch in range(config['num_epochs']):
-        for i in tqdm(range(config['resolution'])):
-            input = inputs[:, i, :].float()
-            target = target_img[:, i, :]
-            target = torch.permute(target, (1, 0))
+        input = inputs.view(-1, 2).to(device).to(dtype) # -1是自动计算维度大小的意思
 
-            # 前向传播
-            output = hashgrid(input)
-            rgb = renderer(output)
+        output = hashgrid(input)
+        rgb = renderer(output)
 
-            # 计算损失
-            loss = F.mse_loss(rgb, target)
+        loss = F.mse_loss(rgb, target)
 
-            # 反向传播和优化
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        torch.cuda.empty_cache()
 
         print(f"Epoch [{epoch + 1}/{config['num_epochs']}], Loss: {loss.item()}")
 
+        if epoch % 1000 == 0:
+            os.makedirs('outputs', exist_ok=True)
+            torch.save({
+                'hashgrid_state_dict': hashgrid.state_dict(),
+                'renderer_state_dict': renderer.state_dict(),}, os.path.join('outputs', 'model.pth'))
+            print("#####################Model saved#####################")
+            
     # 保存模型
     os.makedirs('outputs', exist_ok=True)
     torch.save({
         'hashgrid_state_dict': hashgrid.state_dict(),
         'renderer_state_dict': renderer.state_dict(),
-    }, os.path.join('outputs', 'model.pth'))
+    }, os.path.join('outputs', f"epoch_{config['num_epochs']}model.pth"))
 
 
-# 推理函数
+# 推理
 @torch.no_grad()
-def inference(config):
+def dynamic_res_inference(config,res):
+    torch.cuda.empty_cache()
+
     hashgrid = HashGrid(config['hashgrid_config']).to(device)
     renderer = Renderer.hash_render(config['renderer_config']).to(device)
 
-    checkpoint = torch.load(os.path.join('outputs', 'model.pth'))
+    checkpoint = torch.load(os.path.join('outputs', f"epoch_{config['num_epochs']}model.pth"))
     hashgrid.load_state_dict(checkpoint['hashgrid_state_dict'])
     renderer.load_state_dict(checkpoint['renderer_state_dict'])
-
-    x, y = torch.arange(config['resolution']) / (config['resolution'] - 1) * 2 - 1, torch.arange(config['resolution']) / (config['resolution'] - 1) * 2 - 1
+   
+    x, y = torch.arange(res) / (res - 1) * 2 - 1, torch.arange(res) / (res - 1) * 2 - 1
     x, y = torch.meshgrid(x, y)
-    inputs = torch.stack([x, y], dim=-1).to(device).float()
-    img = torch.zeros((config['resolution'], config['resolution'], 3), device=device)
+    img = torch.zeros((res, res, 3), device=device)
 
-    for i in tqdm(range(config['resolution'])):
-        input = inputs[:, i, :].float()
+    if res <=2048:
+        input = torch.stack([x, y], dim=-1).view(-1, 2).to(device).float()
         output = hashgrid(input)
-        rgb = renderer(output)
+        img = renderer(output).view(res, res, 3)
+        img = img.detach().cpu().numpy()
 
-        img[:, i, :] = rgb
+    
+    else:
+        inputs = torch.stack([x, y], dim=-1).to(device).float()
 
-    # 将 img 从 GPU 移动到 CPU，并转换为 NumPy 数组
-    img = img.detach().cpu().numpy()
-    img = np.flip(img, axis=0)  # 翻转图像以匹配原始图像的方向
-    img = np.rot90(img, k=-1)  # 旋转图像以匹配原始图像的方向
+        for i in tqdm(range(res)):
+            input = inputs[:, i, :].float()
+            output = hashgrid(input)
+            rgb = renderer(output)
+            img[:, i, :] = rgb
+            torch.cuda.empty_cache()
 
-    # 创建输出目录
+        img = img.detach().cpu().numpy()
+
+
     output_dir = 'outputs'
     os.makedirs(output_dir, exist_ok=True)
-
-    # 保存图像到输出目录
     output_path = os.path.join(output_dir, 'output_image.png')
     plt.imsave(output_path, img)
     print(f"Inference result saved to {output_path}")
@@ -121,9 +132,10 @@ def inference(config):
 def main():
     config_path = 'config/config.yaml'
     config = load_config(config_path)
-    train(config)
+    # train(config)
     print("Starting Infenrence...")
-    inference(config)
+    resolution = int(input('Please input the resolution: '))
+    dynamic_res_inference(config,res=resolution)
 
 
 
